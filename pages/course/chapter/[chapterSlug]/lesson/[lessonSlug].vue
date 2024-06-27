df6727a74c2d34a65391e0f9a739036bb1c429f1
Anton Listkov <antonlistkov@gmail.com>
2024-06-27 13:19:34 +0200

    video 7

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -6,6 +6,8 @@
 <template>
   <h2>Lesson</h2>
   <p>This is a lesson</p>
+  <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
+  <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
 </template>
 
 <style scoped>
