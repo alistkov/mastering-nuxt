c9feb90cb1f137f0765bfdb26f6e490ede0c2e37
Anton Listkov <antonlistkov@gmail.com>
2024-08-26 12:06:47 +0200

    32

--- error.vue
+++ error.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-const error = useError()
+  const error = useError();
 
-const handleError = () => {
-  clearError({
-    redirect: '/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3'
-  })
-}
+  const handleError = () => {
+    clearError({
+      redirect:
+        '/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3',
+    });
+  };
 </script>
 
 <template>
   <NuxtLayout>
     <div class="prose">
-      <h1>404</h1>
+      <h1>Dang</h1>
       <p>Something went wrong!</p>
       <p>
         <strong>
@@ -19,7 +20,8 @@ const handleError = () => {
         </strong>
       </p>
       <p>
-        Go to the <a class="hover:cursor-pointer" @click="handleError">first lesson</a>
+        Go to the
+        <a class="hover:cursor-pointer" @click="handleError">first lesson</a>
       </p>
     </div>
   </NuxtLayout>
