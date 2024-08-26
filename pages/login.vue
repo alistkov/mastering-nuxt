27f24b08a90cf27b9e4a9f2c7be1c3f2e9d2f1a9
Anton Listkov <antonlistkov@gmail.com>
2024-08-26 14:05:06 +0200

    commi

--- pages/login.vue
+++ pages/login.vue
@@ -3,6 +3,7 @@
     <h1>Log in to {{ title }}</h1>
     <button
       class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+      @click="login"
     >
       Log in with Github
     </button>
@@ -10,5 +11,16 @@
 </template>
 
 <script setup lang="ts">
-const { title } = useCourse();
+  const { title } = useCourse();
+  const supabase = useSupabaseClient();
+
+  const login = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+    });
+
+    if (error) {
+      console.log(error);
+    }
+  };
 </script>
